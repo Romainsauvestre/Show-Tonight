b9cbb444473f201f4dd882d0b62b96f1e9427933
Romain <sauvestre.romain@gmail.com>
2015-10-04 17:27:01 +0200

    deuxième commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  post 'shows/:id/book' => 'shows#book'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
