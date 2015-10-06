a585cbd448d64ac6d3682c5ebfe21474537d1313
Romain <sauvestre.romain@gmail.com>
2015-10-06 14:33:22 +0200

    enfin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# ruby encoding: utf-8`
+
 
 Show.create(
-    name: "Mon premier Show",
-    venue: "Salle Pleyel",
-    description: "Concert blabla",
+    name: 'Mon premier Show',
+    venue: 'Salle Pleyel',
+    description: 'Concert blabla',
     capacity: 500,
     price: 30,
-    image: "http://www.sallepleyel.fr/img/visuel/diaporama/salle_concert_scene.jpg",
-    date: "2015-10-30"
+    image: 'http://www.sallepleyel.fr/img/visuel/diaporama/salle_concert_scene.jpg',
+    date: '2015-10-30'
 )
 
 Show.create(
-    name: "Sébastien Tellier",
-    venue: "Nouveau Casino",
-    description: "L'Aventura",
+    name: 'Sebastien Tellier',
+    venue: 'Nouveau Casino',
+    description: 'LAventura',
     capacity: 500,
     price: 36,
-    image: "http://www.gqmagazine.fr/uploads/images/201421/cc/l_aventura_de_s__bastien_tellier_7651.jpeg",
-    date: "2015-10-19"
+    image: 'http://www.gqmagazine.fr/uploads/images/201421/cc/l_aventura_de_s__bastien_tellier_7651.jpeg',
+    date: '2015-10-19'
 )
